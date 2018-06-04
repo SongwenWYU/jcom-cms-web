e762eaf0852c1c09490b7285061a912c06983951
songwen <zero.hero.he@gmail.com>
2018-06-04 23:05:34 +0800

    refactor:index page

--- src/main/java/com/sw/jcom/controller/IndexController.java
+++ src/main/java/com/sw/jcom/controller/IndexController.java
@@ -2,7 +2,9 @@ package com.sw.jcom.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +12,12 @@ public class IndexController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/")
-    String index() throws Exception {
+    @Value("system.name")
+    private String systemName;
 
+    @RequestMapping("/")
+    String index(Model model) throws Exception {
+        model.addAttribute("title", "Mango - JCOM");
         return "index";
     }
 
